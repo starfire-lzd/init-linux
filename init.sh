70066c94c40e223eb76cf3fe0adde08d52098298
Li Zhengda <lizhengda@starfire.site>
2017-10-18 13:32:50 +0800

    添加基本功能，没加是否安装的判断

--- init.sh
+++ init.sh
@@ -17,13 +17,19 @@ git clone git://github.com/zsh-users/zsh-autosuggestions
 mv ./zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
 mv ./zsh-syntax-highlighting ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
 #添加oh-my-zsh的主题
-git clone https://github.com/iplaces/astro-zsh-theme.git astro
-mv ./astro/astro.zsh-theme ~/.oh-my-zsh/themes/astro.zsh-theme
+#git clone https://github.com/iplaces/astro-zsh-theme.git astro
+mv ./res/astro.zsh-theme ~/.oh-my-zsh/themes/astro.zsh-theme
 #添加配置文件
 cp ./res/.zshrc ~/.zshrc
 #安装tmux
+sudo apt install tmux
 #安装tmuxen
+git clone git://github.com/xuxiaodong/tmuxen.git
+cd tmuxen
+. ./install-tmuxen
 #安装vim
+sudo apt install vim
 #添加vim的配置文件
+cp ./res/.vimrc ~/.vimrc
 
 
